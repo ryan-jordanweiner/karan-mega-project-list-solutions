a25aa2e5e299b16dc57f78d0ac1e128ea85c95fc
Ryan Weiner <79421575+ryan-jordanweiner@users.noreply.github.com>
2022-05-26 14:18:31 -0400

    Update PI_to_N.cpp
    FINAL SOLUTION TO PROBLEM

--- Numbers/cpp/PI_to_N.cpp
+++ Numbers/cpp/PI_to_N.cpp
@@ -1,9 +1,39 @@
+//INLCUDE REQUIRED HEADER FILES
 #include <iostream>
+#include <iomanip>
+
 using namespace std;
 
 int main()
 {
+  //SET VALUES TO BE USED IN CALCULATIONS
+	float denom = 1;
+	float multiplier = 4;
+	double pi = 0;
+  //CALCULATE PI
+	for (long i = 0; i < 10000000; i++)
+	{
+		if (i%2 != 0)
+		{
+			pi+=1/denom;
+			denom+=2;
+		}
+		else
+		{
+			pi-=1/denom;
+			denom+=2;
+		}
+	}
+	pi*=multiplier;
   
-  
-  return 0;
+	int digits = 0;
+  //GET USER INPUT FOR NUMBER OF DIGITS 
+	cout<<"How many digits of PI would you like to see? "<<endl;
+	cin>>digits;
+  //SET IOSTREAM PRECISION TO DESIGNATED NUMBER
+	cout<<setprecision(digits+1);
+  //OUTPUT
+	cout<<-pi<<endl;
+
+	return 0;
 }
