3d67fe9c55fe46f6ba2560ca968a5d1dddef8092
Ryan Weiner <79421575+ryan-jordanweiner@users.noreply.github.com>
2022-05-26 14:37:12 -0400

    Update PI_to_N.java
    FINAL SOLUTION

--- Numbers/java/PI_to_N.java
+++ Numbers/java/PI_to_N.java
@@ -1,1 +1,39 @@
-//java code
+//IMPORT REQUIRED FILES
+import java.util.Scanner;
+import java.math.BigDecimal;
+
+
+public class Main
+{
+	public static void main(String[] args) 
+	{
+    //CREATE SCANNER OBJECT
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter number of digits of PI you would like to see: ");
+		int digits = scanner.nextInt();
+		float pi  = 0;
+		float denom = 1;
+		float multiplier = 4;
+    
+    //CALCULATE PI
+		for(long i = 0; i < 10000000; i++)
+		{
+			if (i%2!=0)
+			{
+				pi+=1/denom;
+				denom+=2;
+			}
+			
+			else
+			{
+				pi-=1/denom;
+				denom+=2;
+			}
+		}
+		pi*= -multiplier;
+    
+    //PRINT PI TO DESIGNATED PRECISION
+		System.out.println(BigDecimal.valueOf(pi).setScale(digits, BigDecimal.ROUND_HALF_DOWN).floatValue());
+
+	}
+}
